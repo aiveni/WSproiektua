e51705ca1961c25cb274c210e786fd555f02c21b
aiveni <avelaz003@ikasle.ehu.eus>
2022-11-04 18:41:05 +0100

    aldaketa

--- js/ariketa5.js
+++ js/ariketa5.js
@@ -1,14 +1,35 @@
-fetch('premiere.json')
+fetch('premier.json')
 .then(r => r.json())
 .then(data => {
 console.log(data.teams.filter(x=>x.name == 'Arsenal FC'))//Taldea lortzeko.
 
-console.log(data.teams.filter(x=>x.name == 'Arsenal FC').map(a=> {return a.squad.map(b=>{return b.name})}))//Taldearen joaklariak lortzeko.
+let taldeJokalariak=data.teams.filter(x=>x.name == 'Arsenal FC').map(a=> {return a.squad.map(b=>{return b.name})})//Taldearen joaklariak lortzeko.
+console.log(taldeJokalariak)
 
 let lehenengoJokalari=data.teams.filter(x=>x.name == 'Arsenal FC').map(a=>{return a.squad[0]})//Lehenengo jokalariaren datuak lortzeko.
 console.log(lehenengoJokalari)
 
-console.log(data.teams)//5.1 atributu berriak sartu
+//5.1 atributu berriak sartu
+let teamID= (data.teams.map(a=>{return a.id})) //teamId guztiak arrayan sartu
+//console.log(teamID)
+let leagueID=2021
+//console.log(leagueID)
+
+console.log(data)
+
+//data.teams[0].squad[0]+={"teamID":teamID[0]}
+
+//nola sartu jsonean atributuak?
+for(let i=0; i<teamID.length;i++){//20
+
+    for(let z=0; i<data.teams[i].squads.length;z++){//22
+        //data.teams[i].squad[z]={}
+    }
+
+}
+
 
 
 })
+
+premiare.json
