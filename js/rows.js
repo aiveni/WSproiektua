3cdb25d30d654e21230db921076d9e29b531a395
aiveni <avelaz003@ikasle.ehu.eus>
2022-11-07 16:13:41 +0100

    7.1egina

---
+++ js/rows.js
@@ -0,0 +1,17 @@
+//Ariketa 7.1
+function getAge(dateString){
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+//Ariketa 7.2
+function check(theKey,theValue){
+
+    
+}
